ae32357cf5df0933d07dfb7f0adec93961a6ec4d
Cesarfr23 <cesarfernandez860@gmail.com>
2016-08-31 16:12:38 -0400

    Creando Mi Splayer

--- ControlMiniSup/ControlMiniSup/Splayer.cs
+++ ControlMiniSup/ControlMiniSup/Splayer.cs
@@ -16,5 +16,28 @@ namespace ControlMiniSup
         {
             InitializeComponent();
         }
+
+        private void CargaPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Splayer_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (panel1.Width < 352)
+            {
+                panel1.Width = panel1.Width + 20;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                this.Close();
+            }
+        }
     }
 }
